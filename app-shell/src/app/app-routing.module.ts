d416a497247ea53a54584e420b3f8c34a8a737bb
Lucas Moraes <lucas_moraes2011@outlook.com>
2024-07-28 12:20:42 -0300

    fix: adjustments shell

--- app-shell/src/app/app-routing.module.ts
+++ app-shell/src/app/app-routing.module.ts
@@ -1,61 +1,63 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
-import { WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
+import {
+  startsWith,
+  WebComponentWrapperOptions,
+} from '@angular-architects/module-federation-tools';
 import { CustomComponent } from './custom/custom.component';
 import { WebComponentWrapper } from './web-component-wrapper';
-
+import { AppCustomPreloader } from './utils/app-custom-preloader';
 
 const routes: Routes = [
   {
     path: '',
-    component: CustomComponent
+    component: CustomComponent,
   },
   {
     path: 'angular1',
+    data: {
+      preload: true,
+    },
     loadChildren: () =>
       loadRemoteModule({
-          remoteEntry: 'http://localhost:4201/remoteEntry.js',
-          remoteName: 'app1',
-          exposedModule: './Module'
-      })
-      .then(m => m.AppModule)
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        remoteName: 'app1',
+        exposedModule: './Module',
+      }).then((m) => m.AppModule),
   },
   {
+    // matcher: startsWith('teste-b'),
     path: 'teste-b',
     component: WebComponentWrapper,
     data: {
+      preload: true,
       remoteEntry: 'http://localhost:4202/remoteEntry.js',
       exposedModule: './teste-b',
       elementName: 'app-teste-b',
-      remoteName: 'angular1'
-    } as WebComponentWrapperOptions
-},
-{
-  path: 'teste-a',
-  component: WebComponentWrapper,
-  data: {
-    remoteEntry: 'http://localhost:4202/remoteEntry.js',
-    exposedModule: './teste-a',
-    elementName: 'app-teste-a',
-    remoteName: 'angular1'
-  } as WebComponentWrapperOptions
-},
-  // {
-  //   path: 'app2',
-  //   loadChildren: () =>
-  //     loadRemoteModule({
-  //         remoteEntry: 'http://localhost:4202/remoteEntry.js',
-  //         remoteName: 'app2',
-  //         exposedModule: './Module'
-  //     })
-  //     .then(m => m.AppModule)
-  // }
+      remoteName: 'angular1',
+    } as WebComponentWrapperOptions,
+  },
+  {
+    matcher: startsWith('teste-a'),
+    component: WebComponentWrapper,
+    data: {
+      remoteEntry: 'http://localhost:4202/remoteEntry.js',
+      exposedModule: './teste-a',
+      elementName: 'app-teste-a',
+      remoteName: 'angular1',
+    } as WebComponentWrapperOptions,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: AppCustomPreloader,
+    }),
+  ],
+  exports: [RouterModule],
+  providers: [AppCustomPreloader],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
